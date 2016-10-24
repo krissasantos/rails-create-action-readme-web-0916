9cd46e2ce0f553367dd11d6839da296f2bb635a8
krissasantos <krissa.santos31@gmail.com>
2016-10-24 13:56:41 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+
 	def index
 		@posts = Post.all
 	end
@@ -9,5 +11,13 @@ class PostsController < ApplicationController
 
 	def new
 		@post = Post.new
+	end
+
+	def create
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
 end
